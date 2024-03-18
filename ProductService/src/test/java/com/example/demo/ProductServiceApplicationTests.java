3fbdda0e92766c0bdeae845eb00bb73a4478ed36
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2024-03-17 20:37:34 -0500

    test j jupiter

--- ProductService/src/test/java/com/example/demo/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/example/demo/ProductServiceApplicationTests.java
@@ -1,11 +1,25 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0");
+	
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	
 	@Test
 	void contextLoads() {
 	}
