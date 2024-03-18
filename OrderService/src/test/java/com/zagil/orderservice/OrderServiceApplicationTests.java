0a8722d27c5b372488eb51dee41b6f8600765bc9
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2024-03-17 21:36:08 -0500

    feat order-service iniz

---
+++ OrderService/src/test/java/com/zagil/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagil.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
