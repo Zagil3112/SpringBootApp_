29b476d49098bc34e843cc13e1f66a91485f0f5d
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2024-05-05 22:56:01 -0500

    inventory update

--- inventory-service/src/main/java/com/zagil/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/zagil/inventoryservice/repository/InventoryRepository.java
@@ -1,9 +1,13 @@
 package com.zagil.inventoryservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zagil.inventoryservice.model.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long>{
+
+	Optional <Inventory> findBySkuCode();
 
 }
---
+++ inventory-service/src/main/java/com/zagil/inventoryservice/service/InventoryService.java
@@ -0,0 +1,24 @@
+package com.zagil.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zagil.inventoryservice.repository.InventoryRepository;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+	
+	private final InventoryRepository inventoryRepository; 
+	
+	@Transactional(readOnly = true)
+	public boolean isInStock (String skuCode) {
+		
+		return inventoryRepository.findBySkuCode().isPresent();
+		
+	}
+
+}
