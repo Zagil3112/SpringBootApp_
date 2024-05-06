705b638a19414d16b7c52ccb2c081c10362aea52
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2024-05-05 23:58:13 -0500

    inventory update

--- OrderService/src/main/java/com/zagil/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/zagil/orderservice/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zagil.orderservice.dto.OrderLineItemsDto;
 import com.zagil.orderservice.dto.OrderRequest;
